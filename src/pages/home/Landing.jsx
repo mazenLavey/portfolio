9f08a8fbb4cfcabe8c2c05d8a786bd8f918831bd
mazenLavey <mazen.fp@gmail.com>
2023-04-22 21:15:36 +0400

    adding some ui elements

--- src/pages/home/Landing.jsx
+++ src/pages/home/Landing.jsx
@@ -11,14 +11,14 @@ const Landing = ()=> {
     return (
     <section className={`${LandingCSS.wrapper} container`}>
         <header className={LandingCSS.header}>
-            <h1>{lang === "en"? `Joy through \nsimple design.` : `Радость через \nпростой дизайн.`}</h1>
+            <h1>{lang === "en"? `Joy through \nsimple design.` : `Удовольствие от\nпростого дизайна.`}</h1>
             <span>{lang === "en"? "Mazen Al-Madhage / Front-end Developer": "Аль-Мадхаджи Мазен / Front-end разработчик"}</span>
         </header>
         <p className={LandingCSS.about}>
             {lang === "en" ? 
-                "I am a front-end developer with a passion for web development. I have completed the Meta Front-End Developer certification program on Coursera. I have solid skills in HTML, CSS, JavaScript, and React. Moreover, I have hands-on experience with Figma, Sass, Google Analytics, and Firebase. I continue to enhance my skills through free online resources and I am always eager to learn and grow as a developer."
+                "I am a passionate front-end developer. I have completed the Meta Front-End Developer certification program on Coursera. I have solid skills in React, HTML, CSS, JavaScript, and Sass. Moreover, I have hands-on experience with Figma, bootstrap, Google Analytics, and Firebase. I continue to enhance my skills through free online resources and I am always eager to learn and grow as a developer."
                 :
-                "Я являюсь фронтенд-разработчиком. Прошел программу сертификации Meta Front-End Developer на Coursera. Обладаю навыками работы с HTML, CSS, JavaScript и React. Более того, имею практический опыт работы с Figma, Sass, Google Analytics и Firebase. В настоящее время я продолжаю улучшать свои знания при помощи открытых онлайн-ресурсов.  Готов учиться и расти как разработчик."
+                "Я увлеченный front-end разработчик. Прошел программу сертификации Meta Front-End Developer на Coursera. Обладаю навыками работы с React, HTML, CSS, JavaScript и Sass. Более того, имею практический опыт работы с Figma, Bootstrap, Google Analytics и Firebase. В настоящее время я продолжаю улучшать свои знания при помощи открытых онлайн-ресурсов.  Готов учиться и расти как разработчик."
             }
         </p>
 
