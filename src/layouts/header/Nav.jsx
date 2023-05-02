15a51e0c981e99e7f4c3ddc2475b3b30736e7e34
mazenLavey <mazen.fp@gmail.com>
2023-05-02 11:02:33 +0400

    adding Links

--- src/layouts/header/Nav.jsx
+++ src/layouts/header/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = ()=>{
     return (
     <nav className={NavCSS.wrapper}>
         <ul>
-            <li><a href="#projects" rel="noreferrer" title="Go to Projects Section" >{lang === "en"? "projects": "Проекты"}</a></li>
+            <li><a href="https://drive.google.com/file/d/1KYN0-CdpWtqxwOeAFzGHO3AlOOE6Iq4d/view?usp=share_link" rel="noreferrer" title="download resume" target='_blank' >{lang === "en"? "Resume": "Резюме"}</a></li>
             <li>
                 <a href="https://www.linkedin.com/in/mazen-al-madhage" rel="noreferrer" target="_blank" title="Go to Mazen's Linkedin" onClick={handleClick}>
                     <FontAwesomeIcon icon={faLinkedin} style={{fontSize: "var(--font-600)"}}/>
--- src/pages/home/Landing.jsx
+++ src/pages/home/Landing.jsx
@@ -11,16 +11,16 @@ const Landing = ()=> {
     return (
     <section className={`${LandingCSS.wrapper} container`}>
         <header className={LandingCSS.header}>
-            <h1>{lang === "en"? `Joy through \nsimple design.` : `Удовольствие от\nпростого дизайна.`}</h1>
+            <h1>{lang === "en"? `Turning ideas \ninto interactive reality.` : `Превращение идей \nв интерактивную реальность.`}</h1>
             <span>{lang === "en"? "Mazen Al-Madhage / Front-end Developer": "Аль-Мадхаджи Мазен / Front-end разработчик"}</span>
         </header>
-        <p className={LandingCSS.about}>
+        {/* <p className={LandingCSS.about}>
             {lang === "en" ? 
                 "I am a passionate front-end developer. I have completed the Meta Front-End Developer certification program on Coursera. I have solid skills in React, HTML, CSS, JavaScript, and Sass. Moreover, I have hands-on experience with Figma, bootstrap, Google Analytics, and Firebase. I continue to enhance my skills through free online resources and I am always eager to learn and grow as a developer."
                 :
                 "Я увлеченный front-end разработчик. Прошел программу сертификации Meta Front-End Developer на Coursera. Обладаю навыками работы с React, HTML, CSS, JavaScript и Sass. Более того, имею практический опыт работы с Figma, Bootstrap, Google Analytics и Firebase. В настоящее время я продолжаю улучшать свои знания при помощи открытых онлайн-ресурсов.  Готов учиться и расти как разработчик."
             }
-        </p>
+        </p> */}
 
         <span className={LandingCSS.certificate}>
             <CertificateBadge />
